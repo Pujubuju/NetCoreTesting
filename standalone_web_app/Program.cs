1981cd39ff8191f584543249764acc0c921868a7
pujubuju <slawomir.sen@gmail.com>
2016-07-10 15:02:20 +0200

    add mvc and web api controller

--- standalone_web_app/Program.cs
+++ standalone_web_app/Program.cs
@@ -3,6 +3,8 @@ using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConsoleApplication
 {
@@ -21,23 +23,46 @@ namespace ConsoleApplication
 
     public class Startup
     {
+        // public void Configure(IApplicationBuilder app)
+        // {
+        //     app.Use(async (context, next) =>
+        //     {
+        //         await context.Response.WriteAsync("Pre Processing request from: ");
+        //         await context.Response.WriteAsync(context.Connection.LocalIpAddress.ToString());
+        //         await next();
+        //         await context.Response.WriteAsync(" Post Processing. Done.");
+        //     });
+
+        //     app.Run(async (context) =>
+        //     {
+        //         await context.Response.WriteAsync(
+        //             "Hello World. The Time is: " +
+        //             DateTime.Now.ToString("hh:mm:ss tt"));
+
+        //     });
+        // }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Pre Processing request from: ");
-                await context.Response.WriteAsync(context.Connection.LocalIpAddress.ToString());
-                await next();
-                await context.Response.WriteAsync(" Post Processing. Done.");
-            });
+            app.UseMvc();
+        }
+    }
 
-            app.Run(async (context) =>
+    public class HelloWorldController
+    {
+        [HttpGet("api/helloworld")]
+        public object HelloWorld()
+        {
+            return new
             {
-                await context.Response.WriteAsync(
-                    "Hello World. The Time is: " +
-                    DateTime.Now.ToString("hh:mm:ss tt"));
-
-            });
+                message = "Hello World",
+                time = DateTime.Now
+            };
         }
     }
 }
